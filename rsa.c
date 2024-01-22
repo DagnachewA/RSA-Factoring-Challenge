380ce58b53f5a39c52928eae1ef6d8f20c5bb3e7
DagnachewA <wchegnada41@gmail.com>
2024-01-22 11:50:51 -0800

    corr

--- rsa.c
+++ rsa.c
@@ -3,7 +3,7 @@
 /**
  * main - main function
  *
- * Author: Thaoban Abdrasheed
+ * Main function for rsa
  * Return: void
  */
 int main(int argc, char *argv[])
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while((line = getline(&buffer, &count, fptr)) != -1)
+	while ((line = getline(&buffer, &count, fptr)) != -1)
 	{
 		factorize(buffer);
 	}
